b94624dfa9d45fb103bfe752c3a9969fee30dc4a
Darren Rose <darrenwrose@gmail.com>
2021-03-28 19:19:35 +0200

    test: make test fail

--- test_mycamel_unittest.py
+++ test_mycamel_unittest.py
@@ -4,7 +4,7 @@ import unittest
 class TestSum(unittest.TestCase):
 
     def test_toCamel(self):
-        self.assertEqual(mycamel.toCamel("hello world"), "Hello World", "Should be Hello World")
+        self.assertEqual(mycamel.toCamel("hello w orld"), "Hello World", "Should be Hello World")
 
 if __name__ == "__main__":
     unittest.main()
